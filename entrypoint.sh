0eb5eabb33e0d082ce4d29bfd15170ba6a4ecf24
Dat Quach <datquach@kms-technology.com>
2019-11-29 14:32:10 +0700

    fix katalon run recived param

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -l
-#echo "$INPUT_KS_COMMAND -apiKey=$INPUT_KS_API_KEY"
-#katalon-execute.sh $INPUT_KS_COMMAND -apiKey=$INPUT_KS_API_KEY
-
-echo "hello $1 $2"
+katalon-execute.sh $1 -apiKey=$2
 
 status="done"
 echo ::set-output name=status::$status
