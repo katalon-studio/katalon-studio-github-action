5d65cb377ac3cf0c55931860c7c80e038b179178
Dat Quach <datquach@kms-technology.com>
2019-11-29 11:18:37 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 echo "$INPUT_KS_COMMAND -apiKey=$INPUT_KS_API_KEY"
-katalon-execute.sh $1 -apiKey=$2
+katalon-execute.sh $INPUT_KS_COMMAND -apiKey=$INPUT_KS_API_KEY
 status="done"
 echo ::set-output name=status::$status
