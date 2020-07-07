1f0cac1ad44d418e603ba53ddc36eae5233745f5
Alex <dev.alex.88@gmail.com>
2020-07-07 12:15:21 +0700

    update logger

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
-const {execute} = require('katalon-cli/src/katalon-studio');
-const core = require('@actions/core');
+const { execute } = require("katalon-cli/src/katalon-studio");
+const core = require("@actions/core");
 
-const user_version = core.getInput('version');
-const user_projectPath = core.getInput('projectPath');
-const user_args = core.getInput('args');
+const user_version = core.getInput("version");
+const user_projectPath = core.getInput("projectPath");
+const user_args = core.getInput("args");
 
-try
-{
-    execute(
-        user_version,
-        '',
-        user_projectPath,
-        user_args,
-        '',
-        '',
-        {
-            info: function()
-            {
-                console.log(arguments);
-            },
-            debug: function()
-            {
-                console.log(arguments);
-            }
-        }
-    )
-} catch (error)
-{
-    core.setFailed(error.message);
+try {
+  execute(user_version, "", user_projectPath, user_args, "", "", {
+    info: function (message) {
+      console.log(message);
+    },
+    debug: function (message) {
+      console.log(message);
+    },
+    error: function (message) {
+      console.error(message);
+    },
+  });
+} catch (error) {
+  core.setFailed(error.message);
 }
