b3cf518a03638fcf8eef31d2047cdd21d2c65cb7
Alex <dev.alex.88@gmail.com>
2020-07-07 16:11:07 +0700

    update status handling

--- index.js
+++ index.js
@@ -6,28 +6,27 @@ const user_projectPath = core.getInput("projectPath");
 const user_args = core.getInput("args");
 
 try {
-  const status = await execute(
-    user_version,
-    "",
-    user_projectPath,
-    user_args,
-    "",
-    "",
-    {
-      info: function (message) {
-        console.log(message);
-      },
-      debug: function (message) {
-        console.log(message);
-      },
-      error: function (message) {
-        console.error(message);
-      },
-    }
-  );
-  if (status !== 0) {
-    core.setFailed(`Exit code ${status}.`);
-  }
+  execute(user_version, "", user_projectPath, user_args, "", "", {
+    info: function (message) {
+      console.log(message);
+    },
+    debug: function (message) {
+      console.log(message);
+    },
+    error: function (message) {
+      console.error(message);
+    },
+  })
+    .then((status) => {
+      if (status !== 0) {
+        core.setFailed(`Exit code ${status}.`);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      core.setFailed(err);
+    });
 } catch (error) {
+  console.error(error);
   core.setFailed(error.message);
 }
