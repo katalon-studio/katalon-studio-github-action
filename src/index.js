f2b522590d78c3d9c5547c5db77dd1f4ba02d6bf
Vu Than <158494061+thvu-katalon@users.noreply.github.com>
2026-09-04 15:43:38 +0700

    [STUDIO-10975] Update dependencies to more recent versions (#45)

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import * as core from "@actions/core";
+import { execute } from "katalon-agent/src/service/katalon-studio";
+
+const kreVersion = core.getInput("version");
+const projectPath = core.getInput("projectPath");
+const kreArgs = core.getInput("args");
+
+try {
+  execute(kreVersion, "", projectPath, kreArgs, "", "", "", {
+    info: function (message) {
+      console.log(message);
+    },
+    debug: function (message) {
+      console.log(message);
+    },
+    error: function (message) {
+      console.error(message);
+    },
+  })
+    .then((status) => {
+      if (status !== 0) {
+        core.setFailed(`Exit code ${status}.`);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      core.setFailed(err);
+    });
+} catch (error) {
+  console.error(error);
+  core.setFailed(error.message);
+}
